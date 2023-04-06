d3e8e89a073ef6b6d0e6f99735c755c134d6371d
bricep <bricep10@gmail.com>
2023-04-05 22:19:39 -0400

    Create Setup OOO for User via Powershell.ps1

---
+++ Setup OOO for User via Powershell.ps1
@@ -0,0 +1,1 @@
+Set-MailboxAutoReplyConfiguration -Identity user@example.com -AutoReplyState Enabled -ExternalMessage "message with whatever autoreply" -InternalMessage "internal something autoreply" 
