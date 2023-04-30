62b2dc165c666dcb2595c567a3eabc7d9e460707
bricep <bricep10@gmail.com>
2023-04-30 15:31:34 -0400

    Update search_share_for_files_by_age.ps1

--- search_share_for_files_by_age.ps1
+++ search_share_for_files_by_age.ps1
@@ -1,4 +1,5 @@
-#Find Subfolders of a Share With No Files Newer then a Year 
+#Find Subfolders of a Share With No Files Newer then a Year
+#https://serverfault.com/questions/348087/finding-the-most-recent-modification-date-of-all-files-subfolders-in-all-top-lev
 $shareName = "\\server\share"
 # Don't -recurse, just grab top-level directories 
 $directories = Get-ChildItem -Path $shareName | Where-Object { $_.psIsContainer -eq $true } 
